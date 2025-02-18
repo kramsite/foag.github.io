2be1f4dae3d596e4e4c91d8b6ba6bd1d70d72e57
foag <cursotecnicorrkm@gmail.com>
2025-02-18 10:27:54 -0400

    cxc
    xcxzc

--- index/index.js
+++ index/index.js
@@ -1,10 +1,5 @@
 document.getElementById("loginForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita o envio padrão do formulário
-    window.open("../calendario/calendario.html", "_blank"); // Abre a nova aba
-  });
-
-  document.getElementById("loginForm").addEventListener("submit", function(event) {
-    // Obtenha os valores dos campos
+    // Obtenha os valores dos campos de e-mail e senha
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
 
@@ -12,5 +7,8 @@ document.getElementById("loginForm").addEventListener("submit", function(event) 
     if (email.trim() === "" || password.trim() === "") {
         event.preventDefault(); // Impede o envio do formulário
         alert("Por favor, preencha todos os campos.");
+    } else {
+        // Caso os campos sejam preenchidos, o formulário pode ser enviado normalmente
+        // Aqui você pode adicionar a lógica de redirecionamento ou processamento de login
     }
 });
