dc750ab0d49a112d210fb4a7ef1cf2f3fa77b7cf
foag <cursotecnicorrkm@gmail.com>
2025-02-18 08:30:05 -0400

    wqasa
    sasas

--- HORARIO/horario.js
+++ HORARIO/horario.js
@@ -35,8 +35,3 @@ function adicionarIntervalo() {
     celula.style.backgroundColor = "#ffcccb";
     celula.innerHTML = "Intervalo";
 }
-
-function toggleMenu() {
-    const menu = document.getElementById("menuLateral");
-    menu.style.width = menu.style.width === "250px" ? "0" : "250px";
-}
