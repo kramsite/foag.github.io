af64a03f0641161cf4f38ccfff95cd59f1fa2377
foag <cursotecnicorrkm@gmail.com>
2025-09-09 08:27:34 -0400

    .

--- inicio/inicio.php
+++ inicio/inicio.php
@@ -3,11 +3,11 @@
 <head>
   <meta charset="UTF-8" />
   <meta name="viewport" content="width=device-width, initial-scale=1" />
-  <title>Horário Escolar - Sobre</title>
+  <title>FOAG - Inicio</title>
   <link rel="stylesheet" href="inic.css">
   <link rel="stylesheet" href="dark_inic.css">
-  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="stylesheet">
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
+  <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600&family=Roboto:wght@400;500&display=swap" rel="estilo">
+  <link rel="style" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
   <script src="../m.escuro/dark-mode.js"></script>
 </head>
 <body>
