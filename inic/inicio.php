0517dd5d4c05d7cc4cd8d3f7af6fab4ca38db4a1
foag <cursotecnicorrkm@gmail.com>
2025-11-18 10:26:20 -0400

    .

--- inic/inicio.php
+++ inic/inicio.php
@@ -8,7 +8,7 @@ session_start();
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>FOAG - Início</title>
     <link rel="stylesheet" href="inicio.css">
-    <link rel="stylesheet" href="dark_ini.css">
+    <link rel="stylesheet" href="dark_inicio.css">
     <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <script src="../m.escuro/dark-mode.js"></script>
